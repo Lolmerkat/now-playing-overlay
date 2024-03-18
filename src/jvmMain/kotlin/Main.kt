289ad357c6325bf6973f933805078cadd89598a3
Lolmerkat <97191104+Lolmerkat@users.noreply.github.com>
2024-03-18 03:02:35 +0100

    [Fix] Text will now scale correctly, according to the window size

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,19 +1,29 @@
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
-import de.lolmerkat.overlay.gui.Application
+import de.lolmerkat.overlay.gui.TrackWindow
 import de.lolmerkat.overlay.gui.util.SizeManager
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
-@OptIn(DelicateCoroutinesApi::class)
+@OptIn(DelicateCoroutinesApi::class, ExperimentalComposeUiApi::class)
 fun main() = application {
+    var showControlWindow by remember { mutableStateOf(false) }
+
     GlobalScope.launch {
         delay(5000L)
-
     }
 
     Window(
@@ -26,8 +36,33 @@ fun main() = application {
             placement = WindowPlacement.Floating,
             position = WindowPosition(0.dp, 0.dp),
             size = DpSize(SizeManager.appWidth.dp, SizeManager.appHeight.dp)
-        )
+        ),
     ) {
-        Application()
+        TrackWindow(
+            Modifier.onPointerEvent(
+                eventType = PointerEventType.Enter,
+                onEvent = { showControlWindow = true }
+            )
+            .onPointerEvent(
+                eventType = PointerEventType.Exit,
+                onEvent = { showControlWindow = false }
+            ))
+    }
+
+    if (showControlWindow) {
+        Window(
+            onCloseRequest = {},
+            undecorated = true,
+            transparent = false,
+            alwaysOnTop = true,
+            resizable = false,
+            state = WindowState(
+                placement = WindowPlacement.Floating,
+                position = WindowPosition(0.dp, (SizeManager.appHeight + (SizeManager.Padding.outer / 3.0).roundToInt()).dp),
+                size = DpSize(SizeManager.appWidth.dp, (SizeManager.appHeight / 3.0).roundToInt().dp)
+            ),
+        ) {
+
+        }
     }
 }
